8fe5896665fb85d6a4bb3649f4a29d1b7507a577
miya_keitaro <bravegenel@hotmail.co.jp>
2019-03-09 15:35:53 +0900

    短形選択の描画部分を実装

--- MAPCreater.c
+++ MAPCreater.c
@@ -296,8 +296,43 @@ int place_mapchip(SDL_Point mouse_position, SDL_Renderer *renderer) {
 
 int place_mapchip_with_key(SDL_Renderer *renderer) {
 
-    map_array[(cursor.map_y / GRID_SIZE) * COL +
-              (cursor.map_x / GRID_SIZE)] = select_mapchip;
+    int x, y;
+
+    if (short_sharp_selection == SHORT_SHARP_SELECTION_OFF) {
+        map_array[(cursor.map_y / GRID_SIZE) * COL +
+                  (cursor.map_x / GRID_SIZE)] = select_mapchip;
+    } else {
+        if ((cursor.map_x/GRID_SIZE) - (SELECT_X/GRID_SIZE) < 0 &&
+            (cursor.map_y/GRID_SIZE) - (SELECT_Y/GRID_SIZE) < 0 ) {
+            for (x = cursor.map_x/GRID_SIZE;x <= SELECT_X/GRID_SIZE;x++) {
+                for (y = cursor.map_y/GRID_SIZE;y <= SELECT_Y/GRID_SIZE;y++) {
+                    map_array[y * COL + x] = select_mapchip;
+                }
+            }
+        } else if ((cursor.map_x/GRID_SIZE) - (SELECT_X/GRID_SIZE) < 0 &&
+                   (cursor.map_y/GRID_SIZE) - (SELECT_Y/GRID_SIZE) >= 0 ) {
+            for (x = cursor.map_x/GRID_SIZE;x <= SELECT_X/GRID_SIZE;x++) {
+                for (y = SELECT_Y/GRID_SIZE;y <= cursor.map_y/GRID_SIZE;y++) {
+                    map_array[y * COL + x] = select_mapchip;
+                }
+            }
+        } else if ((cursor.map_x/GRID_SIZE) - (SELECT_X/GRID_SIZE) >= 0 &&
+                   (cursor.map_y/GRID_SIZE) - (SELECT_Y/GRID_SIZE) < 0 ) {
+            for (x = SELECT_X/GRID_SIZE;x <= cursor.map_x/GRID_SIZE;x++) {
+                for (y = cursor.map_y/GRID_SIZE;y <= SELECT_Y/GRID_SIZE;y++) {
+                    map_array[y * COL + x] = select_mapchip;
+                }
+            }
+        } else {
+            for (x = SELECT_X/GRID_SIZE;x <= cursor.map_x/GRID_SIZE;x++) {
+                for (y = SELECT_Y/GRID_SIZE;y <= cursor.map_y/GRID_SIZE;y++) {
+                    map_array[y * COL + x] = select_mapchip;
+                }
+            }
+        }
+
+        short_sharp_selection = SHORT_SHARP_SELECTION_OFF;
+    }
 
     return 0;
 
