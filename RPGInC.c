1659c3a35ef17a5fb7c37dafd7a97d41d7ea8ca7
miya_keitaro <bravegenel@hotmail.co.jp>
2019-10-11 00:13:12 +0900

    メッセージ途中のスペースでページ単位での表示に切り替わるようにした

--- RPGInC.c
+++ RPGInC.c
@@ -710,6 +710,7 @@ int message_engine(SDL_Renderer *renderer, TTF_Font *font, SDL_Event e) {
     const int col_size = 88;
 
     char message_tmp[1024];
+    char message_out[100];
     char word[6];
     char isspace[3] = {0};
     char isasterisk[3] = {0};
@@ -794,9 +795,87 @@ int message_engine(SDL_Renderer *renderer, TTF_Font *font, SDL_Event e) {
             word_length = message_length - remaining_message_length;
 
             if ( SDL_PollEvent(&e) ) {
-                if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_SPACE){
-                    printf("%d %s\n", loop_counter, message);
-                    display_character_string(renderer, font, message, 155 + (col_position + byt) * 5 , 354 + row_position);
+                if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_SPACE) {
+                    col_position = col_position + byt;
+                    loop_counter = loop_counter + byt;
+
+                    memset(message_out, '\0', 100);
+
+                    while (*message != '\0') {
+                        byt = u8mb(*message);
+                        message += u8mb(*message);
+
+                        remaining_message_length = strlen(message);
+
+                        memset(word, '\0', 6);
+
+                        byte_counter = 0;
+                        for (x=word_length;x<message_length - remaining_message_length;x++) {
+                            sprintf(&word[byte_counter], "%c", message_tmp[x]);
+                            byte_counter++;
+                        }
+
+                        // 改行判定(半角スペースが2つ続いたら改行とみなす)
+                        if (*word == ' ') {
+                            if (strcmp(isspace, "  ") == 0) {
+                                memset(isspace, '\0', 3);
+                            }
+                            strncat(isspace, word, 1);
+                        } else {
+                            memset(isspace, '\0', 3);
+                        }
+
+                        if (strcmp(isspace, "  ") == 0) {
+                           loop_counter = loop_counter + row_size - (loop_counter % row_size);
+                        }
+
+                        // 改ページ判定(アスタリスクが2つ続いたら改ページとみなす)
+                        if (*word == '*') {
+                            if (strcmp(isasterisk, "**") == 0) {
+                                memset(isasterisk, '\0', 3);
+                            }
+                            strncat(isasterisk, word, 1);
+                            strncpy(word, " ", 1);
+                        } else {
+                            memset(isasterisk, '\0', 3);
+                        }
+
+                        if (strcmp(isasterisk, "**") == 0) {
+                            loop_counter = loop_counter + col_size - (loop_counter % col_size);
+                        }
+
+                        strcat(message_out, word);
+
+                        // 折り返し判定
+                        if (loop_counter % row_size == 0) {
+                            display_character_string(renderer, font, message_out, 155 + col_position * 5 , 354 + row_position);
+                            row_position += row_size;
+                            col_position = 0;
+                            tmp_position = word_length;
+                            memset(message_out, '\0', 100);
+                            if (loop_counter % col_size == 0 ) {
+                                while (1) {
+                                    flash_triangle(renderer);
+
+                                    if ( SDL_PollEvent(&e) ) {
+                                        if (e.type == SDL_KEYDOWN && e.key.keysym.sym == SDLK_SPACE){
+                                            break;
+                                        }
+                                    }
+                                }
+                                make_window(renderer, message_window);
+                                row_position = 0;
+                                col_position = 0;
+                            }
+
+                            loop_counter++;
+                        }
+                        word_length = message_length - remaining_message_length;
+
+                        loop_counter++;
+                    }
+                    display_character_string(renderer, font, message_out, 155 + col_position * 5 , 354 + row_position);
+
                     break;
                 }
             }
